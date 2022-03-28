f047e56bd4718ebb31a02edcda2f76c464810e5a
Falcon028 <1755514977@qq.com>
2022-03-28 22:48:38 +0800

    update existing code

---
+++ src/main/java/com/example/javafxdemo/PrintPageController.java
@@ -0,0 +1,24 @@
+package com.example.javafxdemo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class PrintPageController {
+    @FXML
+    private Label airLineInfo;
+    private Label extraPay;
+    public AnchorPane checkinanchor;
+
+    @FXML
+    protected void onClickPrintMaterialButton() throws IOException {
+
+
+    }
+
+}
