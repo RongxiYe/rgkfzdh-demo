bb87ae08ff6155c85da816555500bbe16f44b609
Rongxi <xuanyewings@163.com>
2022-03-29 19:55:55 +0800

    update main controller

--- src/main/java/com/example/javafxdemo/HelloApplication.java
+++ src/main/java/com/example/javafxdemo/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.example.javafxdemo;
 
+import com.example.javafxdemo.controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
+        AnchorPane root = fxmlLoader.load();
+        MainController main = fxmlLoader.getController();
+        main.init(fxmlLoader);
+        Scene scene = new Scene(root, 900, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setResizable(false);
--- src/main/java/com/example/javafxdemo/controller/user/PrintPageController.java
+++ src/main/java/com/example/javafxdemo/controller/user/PrintPageController.java
@@ -3,18 +3,17 @@ package com.example.javafxdemo.controller.user;
 import com.example.javafxdemo.data.UserData;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Optional;
 
 public class PrintPageController {
     @FXML
-    public Label airLineInfo;
-    public Label extraPay;
+    private Label airLineInfo;
+    private Label extraPay;
     public AnchorPane checkinanchor;
     private UserData userData;
     private FXMLLoader[] loaders;
@@ -22,37 +21,12 @@ public class PrintPageController {
     public void init(UserData data, FXMLLoader[] loaders){
         this.userData = data;
         this.loaders = loaders;
-        //airline information 打印
-        String bn = userData.getBookingNum();
-        String sn = userData.getSurname();
-        String an = userData.getFlightNum();
-        //目前日期没有值，无法转换格式
-       java.text.SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd ");
-        String date = formatter.format(userData.getFlyingDate());;
-        String po = userData.getDelayStatus();
-        String cg = userData.getLuggage();
-
-        //payment 打印
-        String stn = userData.getSeatNum();
-        String ml = userData.getMeal();
-
-        String string1 = bn+"\n"+"\n"+sn+"\n"+"\n"+an+"\n"+"\n"+date+"\n"+"\n"+po+"\n"+"\n"+cg;
-        String string2 = stn+"\n"+"\n"+"\n"+ml;
-        //airLineInfo和extraPay换行显示data
-        airLineInfo.setText(string1);
-        extraPay.setText(string2);
     }
 
     @FXML
     protected void onClickPrintMaterialButton() throws IOException {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION); //
-        alert.setHeaderText("Confirmation");
-        alert.setContentText("Are you sure to print all your materials?");
-        alert.showAndWait();
 
-        //打印出信息文件
-        //生成发给后台系统的文件
+
     }
-
 
 }
