49771a863f0edc2a40523593b22fe1f93135f840
Rongxi <xuanyewings@163.com>
2022-03-30 15:41:51 +0800

    add exit and go back function

--- src/main/java/com/example/javafxdemo/controller/MainController.java
+++ src/main/java/com/example/javafxdemo/controller/MainController.java
@@ -6,15 +6,22 @@ import com.example.javafxdemo.data.UserData;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class MainController {
     @FXML
     public AnchorPane mainanchor;
+    public Button exit;
+    public Button help;
+    public Button back;
     private FXMLLoader[] loaders;
     private UserData userData;
+    private FXMLLoader currentLoader;
 
 
     public void init(FXMLLoader mainLoader) throws IOException{
@@ -24,6 +31,7 @@ public class MainController {
         loaders[0] = mainLoader;
         loaders[1] = helloLoader;
         loaders[2] = printLoader;
+        //还有很多其他的页面都在这里初始化
         userData = new UserData();
 //        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         AnchorPane root = helloLoader.load();
@@ -31,15 +39,53 @@ public class MainController {
         HelloController hello = helloLoader.getController();
         hello.init(userData,loaders);
         mainanchor.getChildren().add(root);
+        int num = mainanchor.getChildren().size();
+        mainanchor.getChildren().get(num-2).setVisible(false);
     }
 
     public void loadRoot(FXMLLoader loader) {
         AnchorPane root = loader.getRoot();
+        currentLoader = loader;
         int num = mainanchor.getChildren().size();
+        if (loader==loaders[1]){
+            mainanchor.getChildren().get(num-2).setVisible(false);
+        }else{
+            mainanchor.getChildren().get(num-2).setVisible(true);
+        }
         mainanchor.getChildren().remove(num-1);
         mainanchor.getChildren().add(root);
     }
 
+    @FXML
+    public void exit(){
+        Alert alert = new Alert(Alert.AlertType.WARNING,"Are you sure to exit?");
+        alert.showAndWait();
+        System.exit(0);
+    }
 
+    @FXML
+    public void help(){
 
+    }
+
+    @FXML
+    public void back(){
+        int index = findIndex();
+        if (index==-1){
+            System.out.println("Error!");
+            System.exit(1);
+        }else{
+            loadRoot(loaders[index-1]);
+        }
+
+    }
+
+    public int findIndex(){
+        for (int i = 0; i < 20; i++) {
+            if (loaders[i]==currentLoader){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
