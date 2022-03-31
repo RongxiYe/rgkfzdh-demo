92775fd31fe403062edf781dbad189b3476d11ef
Sabrina <jp2019213519@qmul.ac.uk>
2022-03-31 19:57:57 +0800

    Changes in UserData, PrintPageController, checkin-view.fxml

--- src/main/java/com/example/javafxdemo/controller/user/PrintPageController.java
+++ src/main/java/com/example/javafxdemo/controller/user/PrintPageController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Label;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 public class PrintPageController {
@@ -20,10 +22,25 @@ public class PrintPageController {
     public void init(UserData data, FXMLLoader[] loaders){
         this.userData = data;
         this.loaders = loaders;
+        //airline information 打印
+        String bn = userData.getBookingNum();
+        String sn = userData.getSurname();
+        String an = userData.getFlightNum();
+        //目前日期没有值，无法转换格式
+       java.text.SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd ");
+        String date = formatter.format(userData.getFlyingDate());;
+        String po = userData.getDelayStatus();
+        String cg = userData.getLuggage();
 
+        //payment 打印
+        String stn = userData.getSeatNum();
+        String ml = userData.getMeal();
+
+        String string1 = bn+"\n"+"\n"+sn+"\n"+"\n"+an+"\n"+"\n"+date+"\n"+"\n"+po+"\n"+"\n"+cg;
+        String string2 = stn+"\n"+"\n"+"\n"+ml;
         //airLineInfo和extraPay换行显示data
-        airLineInfo.setText(String.valueOf(userData));
-        extraPay.setText(String.valueOf(userData));
+        airLineInfo.setText(string1);
+        extraPay.setText(string2);
     }
 
     @FXML
