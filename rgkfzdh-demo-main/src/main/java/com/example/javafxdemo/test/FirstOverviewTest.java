7cd4363dafc2cca87d6f65d1619af099a5c6ae96
ritacrthh <101002813+ritacrthh@users.noreply.github.com>
2022-04-09 01:29:56 +0800

    Add files via upload

---
+++ rgkfzdh-demo-main/src/main/java/com/example/javafxdemo/test/FirstOverviewTest.java
@@ -0,0 +1,16 @@
+package com.example.javafxdemo.test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FirstOverviewTest extends Application{
+
+public static void main(String[]args){
+        launch(args);
+        }
+
+@Override
+public void start(Stage primaryStage){
+
+        }
+        }
