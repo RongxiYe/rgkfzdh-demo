936c61cb53dbba1c548ab4339fca8fd35deaf23c
Rongxi <xuanyewings@163.com>
2022-04-08 15:15:14 +0800

    update seat and meal confirmation framework

--- src/main/java/com/example/javafxdemo/controller/user/SmConfirmController.java
+++ src/main/java/com/example/javafxdemo/controller/user/SmConfirmController.java
@@ -3,13 +3,22 @@ package com.example.javafxdemo.controller.user;
 import com.example.javafxdemo.data.UserData;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
 public class SmConfirmController {
+
     @FXML
+    private Button inputInfo;
+    @FXML
+    private Label seatInfo;
+    @FXML
+    private Label mealInfo;
     public AnchorPane smConfirmAnchor;
+
     private FXMLLoader[] loaders;
     private UserData userData;
     private FXMLLoader currentLoader;
@@ -17,6 +26,17 @@ public class SmConfirmController {
     public void init(UserData data, FXMLLoader[] loaders){
         this.userData = data;
         this.loaders = loaders;
+        //show page information depending on whether user has extra payments.
+        if(true){
+
+        }else{
+
+        }
     }
+
+    @FXML public void onClickInputButton(){
+        //use loaders to enter the next page.
+    }
+
 
 }
