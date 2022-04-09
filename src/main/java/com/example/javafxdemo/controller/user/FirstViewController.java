6dfda98d3bdf8ac193c11441450e124954907f48
Sabrina <jp2019213519@qmul.ac.uk>
2022-04-09 18:36:22 +0800

    Changes in first-view.fxml, FirstOverviewTest, FirstViewController

--- src/main/java/com/example/javafxdemo/controller/user/FirstViewController.java
+++ src/main/java/com/example/javafxdemo/controller/user/FirstViewController.java
@@ -1,4 +1,46 @@
 package com.example.javafxdemo.controller.user;
 
+import com.example.javafxdemo.data.UserData;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.io.IOException;
+
 public class FirstViewController {
+    @FXML
+    public AnchorPane firstViewAnchor;
+    private FXMLLoader[] loaders;
+    private UserData userData;
+    private FXMLLoader currentLoader;
+    public Label infoContent;
+    public Label destination;
+
+    public void init(UserData data, FXMLLoader[] loaders){
+        this.userData = data;
+        this.loaders = loaders;
+        //airline information 打印
+        String bn = userData.getBookingNum();
+        String sn = userData.getSurname();
+        String fn = userData.getFirstname();
+        String an = userData.getFlightNum();
+        String dep = userData.getDepAddr();
+        //目前日期没有值，无法转换格式
+        java.text.SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd ");
+        String date = formatter.format(userData.getFlyingDate());
+        String po = userData.getDelayStatus();
+        String cg = userData.getLuggage();
+
+        //payment 打印
+        String des = userData.getDesAddr();
+
+        String string1 = sn+"\n"+"\n\n"+fn+"\n"+"\n"+an+"\n"+"\n"+dep+"\n"+"\n"+date+"\n"+"\n\n"+cg+"\n"+"\n\n"+po;
+        String string2 = des;
+        //airLineInfo和extraPay换行显示data
+        infoContent.setText(string1);
+        destination.setText(string2);
+    }
+
 }
--- src/main/java/com/example/javafxdemo/test/FirstOverviewTest.java
+++ src/main/java/com/example/javafxdemo/test/FirstOverviewTest.java
@@ -3,6 +3,7 @@ package com.example.javafxdemo.test;
 import com.example.javafxdemo.HelloApplication;
 import com.example.javafxdemo.controller.MainController;
 import com.example.javafxdemo.controller.login.HelloController;
+import com.example.javafxdemo.controller.user.FirstViewController;
 import com.example.javafxdemo.controller.user.SmConfirmController;
 import com.example.javafxdemo.data.UserData;
 import javafx.application.Application;
@@ -23,20 +24,26 @@ public static void main(String[]args){
 public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
         FXMLLoader smLoader = new FXMLLoader(HelloApplication.class.getResource("sm-confirm.fxml"));
+        FXMLLoader fvLoader = new FXMLLoader(HelloApplication.class.getResource("first-view.fxml"));
         AnchorPane root = fxmlLoader.load();
         MainController main = fxmlLoader.getController();
         AnchorPane smroot = smLoader.load();
+        AnchorPane fvroot =fvLoader.load();
 
         UserData userData = new UserData();
         FXMLLoader[] loaders = new FXMLLoader[20];
 
-        SmConfirmController smc = smLoader.getController();
-        smc.init(userData,loaders);
-        root.getChildren().add(smroot);
+//        SmConfirmController smc = smLoader.getController();
+//        smc.init(userData,loaders);
+//        root.getChildren().add(smroot);
+//        int num = root.getChildren().size();
+//        root.getChildren().get(num-2).setVisible(false);
+
+        FirstViewController fvc = fvLoader.getController();
+        fvc.init(userData,loaders);
+        root.getChildren().add(fvroot);
         int num = root.getChildren().size();
         root.getChildren().get(num-2).setVisible(false);
-
-
 
         Scene scene = new Scene(root, 900, 600);
         stage.setTitle("Hello!");
