9e25d84d564267a69d457e26557046a3d7ec3009
Qing <1019734934@qq.com>
2022-04-12 14:45:40 +0800

    complete structure changes once included seat-selection function

--- src/main/java/com/example/javafxdemo/controller/user/SeatController.java
+++ src/main/java/com/example/javafxdemo/controller/user/SeatController.java
@@ -4,12 +4,17 @@ import com.example.javafxdemo.controller.MainController;
 import com.example.javafxdemo.data.UserData;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 import java.io.IOException;
 
 public class SeatController{
         @FXML
         private UserData userData;
         private FXMLLoader[] loaders;
+        private String seatNum;
 
         public void init(UserData data, FXMLLoader[] loaders) {
                 this.userData = data;
@@ -23,4 +28,11 @@ public class SeatController{
                 MainController main = loaders[0].getController();
                 main.loadRoot(loaders[2]);
         }
+
+        public void onClickSeat(MouseEvent event) throws IOException {
+                Rectangle rect = (Rectangle) event.getSource();
+                rect.setFill(Color.AQUA);
+                userData.setSeatNum(rect.getId());
+        }
+
 }
