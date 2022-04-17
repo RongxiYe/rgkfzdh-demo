ce0ded675e757bc9d0f377c8800d9682afa6b1e4
Rongxi <xuanyewings@163.com>
2022-04-17 20:35:48 +0800

    add junit 5 and an entry for javafx package

---
+++ src/main/java/com/example/javafxdemo/Entry.java
@@ -0,0 +1,9 @@
+package com.example.javafxdemo;
+
+import javafx.application.Application;
+public class Entry {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
+
