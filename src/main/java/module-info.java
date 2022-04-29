afeda7a137c5a8f3f4304b99fcc38f8c25188e2d
Qing <1019734934@qq.com>
2022-04-29 22:43:40 +0800

    非常简略的加了首页，还有很多问题，仅实现了跳转，先传一次（因为我想拉最新的代码了23333

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,6 +14,8 @@ module com.example.javafxdemo {
 
     opens com.example.javafxdemo to javafx.fxml, javafx.graphics;
     exports com.example.javafxdemo;
+    exports com.example.javafxdemo.controller.FirstPage;
+    opens com.example.javafxdemo.controller.FirstPage to javafx.fxml;
     exports com.example.javafxdemo.controller.login;
     opens com.example.javafxdemo.controller.login to javafx.fxml;
     exports com.example.javafxdemo.controller.user;
