0e967d879980cd81724f8c65c4e2b88d86925f48
Qing <1019734934@qq.com>
2022-04-29 23:07:42 +0800

    帮崔若彤传的：当乘客点击需额外支付费用的餐食按钮，更改userdata中的meal的值

--- src/main/java/com/example/javafxdemo/controller/user/MealController.java
+++ src/main/java/com/example/javafxdemo/controller/user/MealController.java
@@ -3,6 +3,7 @@ package com.example.javafxdemo.controller.user;
 import com.example.javafxdemo.controller.Controller;
 import com.example.javafxdemo.controller.Handler;
 import com.example.javafxdemo.controller.MainController;
+import com.example.javafxdemo.data.CurrentData;
 import com.example.javafxdemo.utils.Page;
 import com.example.javafxdemo.utils.UserData;
 import javafx.fxml.FXML;
@@ -39,19 +40,25 @@ public class MealController implements Controller {
     }
 
     @FXML public void onClickChooseStaple(){
-        //令。。。等于normal,修改json文件
+        //令。。。等于normal,修改userdata
         mealselection = "normal";
-
+        UserData userData = CurrentData.userData;
+        userData.setMeal(mealselection);
     }
 
     @FXML public void onClickChooseHalal(){
         //令。。。等于halal
         mealselection = "halal";
+        UserData userData = CurrentData.userData;
+        userData.setMeal(mealselection);
     }
 
-    @FXML public void onClickChooseDAD(){
-        //令。。。+ DAD & extra payment
-        mealselection = "DAD (extra payment)";
+    @FXML public void onClickChooseSET(){
+        //如果选择了套餐，则需要额外加钱，先将值赋给meal，
+        //跳转到smconfirm界面后，通过判断值是否为extra，如果是extra，则在smconfirm界面计算金额
+        mealselection = "extra";
+        UserData userData = CurrentData.userData;
+        userData.setMeal(mealselection);
     }
 
 
