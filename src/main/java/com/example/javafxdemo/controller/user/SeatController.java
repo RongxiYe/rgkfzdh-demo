64121e51c9a4f0d346740d9be9a2b29d8608929c
Qing <1019734934@qq.com>
2022-05-24 20:52:27 +0800

    把seat部分识别为空的代码改了一点点

--- src/main/java/com/example/javafxdemo/controller/user/SeatController.java
+++ src/main/java/com/example/javafxdemo/controller/user/SeatController.java
@@ -106,7 +106,7 @@ public class SeatController implements Controller {
                                 } else {
                                         lastRect.setFill(Color.web("#b8bbde"));
                                 }
-                                tempSeatNum = rect.getId();
+                                tempSeatNum = null;
                         }
                         lastRect = rect;
                 }
